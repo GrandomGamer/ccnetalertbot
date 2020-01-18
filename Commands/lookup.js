d16d175e2083bd9192d85fedc42d1ce779ca44fc
GrandomGamer <47879478+GrandomGamer@users.noreply.github.com>
2020-01-17 22:33:57 -0500

    Update lookup.js

--- Commands/lookup.js
+++ Commands/lookup.js
@@ -2,15 +2,16 @@ const request = require("request");
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
+    const sender = message.author
     request("https://map.ccnetmc.com/standalone/dynmap_earth.json?", (error, response, body) => {
         const data = JSON.parse(body);
         let arr = data.players
         for(var i in arr){
             if ((arr[i].account.toUpperCase()) || (arr[i].nickname.toUpperCase()) == args[1]){
-                message.author.send(`X Coordinate: ${arr[i].x}\nY Coordinate: ${arr[i].y}\nZ Coordinate: ${arr[i].z}`)
+                sender.send(`X Coordinate: ${arr[i].x}\nY Coordinate: ${arr[i].y}\nZ Coordinate: ${arr[i].z}`)
             }
             else{
-                message.author.send(`Player not found! :cry:`)
+                sender.send(`Player not found! :cry:`)
             }
 
         }
