a11865da2bb5a797efd511d37e653fc4c7e00681
GrandomGamer <47879478+GrandomGamer@users.noreply.github.com>
2020-01-23 22:49:37 -0500

    Add files via upload

---
+++ Commands/townlist.js
@@ -0,0 +1,81 @@
+const request = require("request");
+const Trello = require("trello")
+var trello = new Trello("b5e7ea4b49b2dace06743a4f27cb96f4","391ff1f537dc143f7ef6d24c7ef4fa69547dea8e44143197224744916fa26d2c")
+var board = 'CcPX6dI1';
+let running = false
+module.exports.run = async (message, args) => {
+    if(running == false){
+        let part2 = false
+        let n = 0
+        run()
+        async function run(){
+            request("https://map.ccnetmc.com/standalone/dynmap_earth.json?",async function(error, response, body){
+                const data = JSON.parse(body);
+                let arr = data.updates
+                let n = 0
+                running = true
+                for(var x in arr){
+                    n++;
+                    if(arr[x].msg === 'markerupdated'){
+                        let desc = arr[x].desc
+                        let chunks = desc.split('>')
+                        let precountry = chunks[4].split(' ')
+                        let country = precountry[1].split(')')[0].substr(0,precountry[1].length-5)
+                        if(country === 'Mexico'){
+                            let pretown = arr[x].label.split(' ')
+                            let town = pretown[0]
+                            let mayor = chunks[14].substr(0,chunks[14].length-6)
+                            let memchunks = chunks[18].split(', ')
+                            let premembers = memchunks.toString()
+                            let members = premembers.substr(0,premembers.length-6)
+                            let xcoord = arr[x].x
+                            let zcoord = arr[x].z
+                            let z = 0;
+                            let lists = trello.getListsOnBoard(board);
+                            lists.then((lists2) =>{
+                                for(var i in lists2){
+                                    z+=1;
+                                    if(lists2[i]['name'] === town){
+                                        let cards = trello.getCardsOnList(lists2[i]['id'])
+                                        cards.then((cards2) =>{
+                                        trello.updateCardDescription(cards2[0]['id'], mayor)
+                                        trello.updateCardDescription(cards2[1]['id'], members)
+                                        trello.updateCardDescription(cards2[2]['id'], `${xcoord} ${zcoord}`)
+                                        })
+                                        return;
+                                    }
+                                    else if(lists2.length+1 === z+1){
+                                        trello.addListToBoard(board, town,
+                                            async function (error, AddList) {
+                                                if (error) {
+                                                    console.log('Could not add list:', error);
+                                                }
+                                                else {
+                                                    console.log('Added list');
+                                                    await trello.addCard('Mayor', mayor, AddList['id'])
+                                                    await trello.addCard('Members', members, AddList['id'])
+                                                    await trello.addCard('Homeblock', `${xcoord} ${zcoord}`, AddList['id'])
+                                                }
+                                            })
+                                        return;
+                                    }
+                                }
+                            part2 = true
+                            })
+                        }
+                    }
+                    else if (n === arr.length){
+                        part2 = true
+                    }
+                }
+                if((arr.length===n) && (part2==true)){
+                    running = false
+                }
+            })
+        }
+    }
+}
+
+module.exports.help = {
+    name: "town"
+}
